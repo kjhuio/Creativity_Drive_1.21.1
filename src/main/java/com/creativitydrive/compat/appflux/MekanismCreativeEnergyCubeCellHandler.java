0bc66e13a70e4929cf6bd582e038c1198b0479a8
qwe0412_ <82296899+kjhuio@users.noreply.github.com>
2026-08-23 14:29:49 +0900

    feat: クリエエネルギーキューブ関連の構築&有効化する準備

--- src/main/java/com/creativitydrive/compat/appflux/MekanismCreativeEnergyCubeCellHandler.java
+++ src/main/java/com/creativitydrive/compat/appflux/MekanismCreativeEnergyCubeCellHandler.java
@@ -1,4 +1,33 @@
 package com.creativitydrive.compat.appflux;
 
-public class MekanismCreativeEnergyCubeCellHandler {
+import appeng.api.storage.cells.ICellHandler;
+import appeng.api.storage.cells.ISaveProvider;
+import appeng.api.storage.cells.StorageCell;
+import com.creativitydrive.ae2.MekanismCreativeFluidTankStorageCell;
+import mekanism.common.item.block.ItemBlockEnergyCube;
+import mekanism.common.tier.EnergyCubeTier;
+import net.minecraft.world.item.ItemStack;
+
+public class MekanismCreativeEnergyCubeCellHandler implements ICellHandler {
+    public static final MekanismCreativeEnergyCubeCellHandler INSTANCE = new MekanismCreativeEnergyCubeCellHandler();
+
+    private MekanismCreativeEnergyCubeCellHandler() {
+    }
+
+    @Override
+    public boolean isCell(ItemStack stack) {return isCreativeEnergyCell(stack);}
+
+    @Override
+    public StorageCell getCellInventory(ItemStack stack, ISaveProvider saveProvider) {
+        if (!isCreativeEnergyCell(stack)) {
+            return null;
+        }
+        return  new MekanismCreativeEnergyCubeStorageCell(stack);
+    }
+
+    private static boolean isCreativeEnergyCell(ItemStack stack) {
+        return !stack.isEmpty()
+                && stack.getItem() instanceof ItemBlockEnergyCube energyCube
+                && energyCube.getTier() == EnergyCubeTier.CREATIVE;
+    }
 }
--- src/main/java/com/creativitydrive/compat/appflux/MekanismCreativeEnergyCubeStorageCell.java
+++ src/main/java/com/creativitydrive/compat/appflux/MekanismCreativeEnergyCubeStorageCell.java
@@ -1,4 +1,83 @@
 package com.creativitydrive.compat.appflux;
 
-public class MekanismCreativeEnergyCubeStorageCell {
+import appeng.api.config.Actionable;
+import appeng.api.networking.security.IActionSource;
+import appeng.api.stacks.AEKey;
+import appeng.api.stacks.KeyCounter;
+import appeng.api.storage.MEStorage;
+import appeng.api.storage.cells.CellState;
+import appeng.api.storage.cells.StorageCell;
+import com.glodblock.github.appflux.common.me.key.FluxKey;
+import com.glodblock.github.appflux.common.me.key.type.EnergyType;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+
+public final class MekanismCreativeEnergyCubeStorageCell implements StorageCell {
+    private static final long DISPLAYED_AMOUNT = Long.MAX_VALUE / 4;
+
+    private final Component description;
+    private final AEKey storedKey;
+
+    public MekanismCreativeEnergyCubeStorageCell(ItemStack stack) {
+        this.description = stack.getHoverName();
+
+        // Applied FluxのFE用AEKeyを作る
+        this.storedKey = FluxKey.of(EnergyType.FE);
+    }
+
+    @Override
+    public long insert(AEKey what, long amount, Actionable mode, IActionSource source) {
+        MEStorage.checkPreconditions(what, amount, mode, source);
+
+        if (!storedKey.equals(what)) {
+            return 0;
+        }
+
+        return amount;
+    }
+
+    @Override
+    public long extract(AEKey what, long amount, Actionable mode, IActionSource source) {
+        MEStorage.checkPreconditions(what, amount, mode, source);
+
+        if (!storedKey.equals(what)) {
+            return 0;
+        }
+
+        return amount;
+    }
+
+    @Override
+    public void getAvailableStacks(KeyCounter out) {
+        out.add(storedKey, DISPLAYED_AMOUNT);
+    }
+
+    @Override
+    public boolean isPreferredStorageFor(AEKey what, IActionSource source) {
+        return storedKey.equals(what);
+    }
+
+    @Override
+    public CellState getStatus() {
+        return CellState.TYPES_FULL;
+    }
+
+    @Override
+    public double getIdleDrain() {
+        return 0;
+    }
+
+    @Override
+    public boolean canFitInsideCell() {
+        return false;
+    }
+
+    @Override
+    public Component getDescription() {
+        return description;
+    }
+
+    @Override
+    public void persist() {
+    }
 }
